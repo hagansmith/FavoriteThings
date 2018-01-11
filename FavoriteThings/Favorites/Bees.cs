fabc4295e7aafac047ce06df3ee3090ab774fc9f
Adam Smith <hagansmith@gmail.com>
2018-01-11 10:25:39 -0600

    all the files so far

---
+++ FavoriteThings/Favorites/Bees.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FavoriteThings.Favorites
+{
+
+    public class Bees
+    {
+        protected string _beeName;
+        
+        public string BeeName
+        {
+            get
+            {
+                return _beeName;
+            }
+            set
+            {
+                _beeName = rocky;
+            }
+        }
+    }
+}
---
+++ FavoriteThings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FavoriteThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var favoriteBee = new Bees();
+
+            Console.WriteLine(favoriteBee.BeeName);
+        }
+    }
+
+     public class Bees
+    {
+        protected string _beeName;
+        
+        public string BeeName
+        {
+            get
+            {
+                return $"My bee's name is {_beeName}";
+            }
+            set
+            {
+               var _beeName = "rocky";
+            }
+        }
+    }
+}
