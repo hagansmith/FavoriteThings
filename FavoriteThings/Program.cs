8fd6d347773710b89903aaae404f4a1c4ddd4963
Adam Smith <hagansmith@gmail.com>
2018-01-12 15:57:58 -0600

    add first favorite thing series.

--- FavoriteThings/Program.cs
+++ FavoriteThings/Program.cs
@@ -2,30 +2,47 @@
 
 namespace FavoriteThings
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var favoriteBee = new Bees();
-
-            Console.WriteLine(favoriteBee.BeeName);
-        }
-    }
-
-     public class Bees
+    public class Bees
     {
         protected string _beeName;
-        
+        protected int _beeAge;
+
         public string BeeName
         {
             get
             {
-                return $"My bee's name is {_beeName}";
+                return _beeName;
             }
             set
             {
-               var _beeName = "rocky";
+                _beeName = value;
             }
         }
+
+        public string BeeAge { get; set; }
+
+        //public int CalculateAge(int age)
+        //{
+        //    int beesAge = _beeAge * 7;
+        //    return beesAge;
+        //}
+        
+        public Bees (string name, int age)
+        {
+            //int beesAge = CalculateAge(age);
+            Console.WriteLine($"My Bee's name is {name}, and he is {age} days old");
+        }
     }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Bees("Rocky", 2);
+
+            Console.ReadKey();
+        }
+    }
+
+
 }
